6f342baa007671d0058b5fe900bcf5686da80e5d
LimMinKyo <story1400@naver.com>
2023-11-22 16:14:48 +0900

    DEL: accounts module

--- src/accounts/dtos/update-account.dto.ts
+++
@@ -1,4 +0,0 @@
-export interface UpdateAccountRequest {
-  accountId: number;
-  name: string;
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MailModule } from './mail/mail.module';
-import { AccountsModule } from './accounts/accounts.module';
 import { APP_FILTER } from '@nestjs/core';
 import { DividendsModule } from './dividends/dividends.module';
 import CatchExceptionFilter from './common/filters/catch-exception.filter';
@@ -38,7 +37,6 @@ import Joi from 'joi';
     UsersModule,
     AuthModule,
     MailModule,
-    AccountsModule,
     DividendsModule,
   ],
   providers: [{ provide: APP_FILTER, useClass: CatchExceptionFilter }],
