ea39319d062215d6521b95c66eda742077626032
LimMinKyo <story1400@naver.com>
2023-12-05 18:51:18 +0900

    ADD: UsersController (e2e) 추가

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -7,6 +7,9 @@ import { CreateDividendRequest } from '@/dividends/dto/create-dividend.dto';
 import { User, Dividend } from '@prisma/client';
 import { JwtService } from '@nestjs/jwt';
 import { UpdateDividendRequest } from '@/dividends/dto/update-dividend.dto';
+import { CreateAccountRequest } from '@/users/dto/create-account.dto';
+import { MailService } from '@/mail/mail.service';
+import { GetDividendsRequest } from '@/dividends/dto/get-dividends.dto';
 
 describe('DividendController (e2e)', () => {
   let app: INestApplication;
@@ -48,7 +51,7 @@ describe('DividendController (e2e)', () => {
     user = await prisma.user.create({
       data: {
         name: '홍길동',
-        email: 'test@test.com',
+        email: 'dividends@test.com',
         provider: 'KAKAO',
       },
     });
@@ -56,7 +59,7 @@ describe('DividendController (e2e)', () => {
     otherUser = await prisma.user.create({
       data: {
         name: '김철수',
-        email: 'test1@test.com',
+        email: 'dividends1@test.com',
         provider: 'LOCAL',
       },
     });
@@ -88,9 +91,14 @@ describe('DividendController (e2e)', () => {
 
   describe('/api/dividends (GET)', () => {
     it('Success (200)', async () => {
+      const getDividendsRequest: GetDividendsRequest = {
+        date: '2023-11',
+      };
+
       const { status, body } = await request(app.getHttpServer())
         .get('/api/dividends')
-        .auth(accessToken, { type: 'bearer' });
+        .auth(accessToken, { type: 'bearer' })
+        .query(getDividendsRequest);
 
       expect(status).toBe(200);
       expect(body).toEqual({
@@ -204,6 +212,127 @@ describe('DividendController (e2e)', () => {
       expect(body).toEqual({
         ok: true,
       });
+
+      expect(count).toBe(0);
+    });
+  });
+});
+
+describe('UsersController (e2e)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let user: User;
+  let accessToken: string;
+
+  const mockMailService = () => ({
+    sendVerificationEmail: jest.fn(),
+  });
+
+  const createAccountRequest: CreateAccountRequest = {
+    name: '홍길동',
+    email: 'users@test.com',
+    password: '12345',
+  };
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider(MailService)
+      .useValue(mockMailService())
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+    prisma = app.get(PrismaService);
+    const jwtService = app.get(JwtService);
+
+    await app.init();
+
+    user = await prisma.user.create({
+      data: {
+        name: '김철수',
+        email: 'users1@test.com',
+        provider: 'LOCAL',
+      },
+    });
+
+    accessToken = jwtService.sign({ id: user.id });
+  });
+
+  afterAll(async () => {
+    await prisma.truncate();
+    await prisma.resetSequences();
+    await prisma.$disconnect();
+    await app.close();
+  });
+
+  describe('/api/users (POST)', () => {
+    it('Created (201)', async () => {
+      const { status, body } = await request(app.getHttpServer())
+        .post('/api/users')
+        .send(createAccountRequest);
+
+      expect(status).toBe(201);
+      expect(body).toEqual({
+        ok: true,
+      });
+
+      const count = await prisma.user.count();
+
+      expect(count).toBe(2);
+    });
+  });
+
+  describe('/api/users/profile (GET)', () => {
+    it('Success (200)', async () => {
+      const { status, body } = await request(app.getHttpServer())
+        .get('/api/users/profile')
+        .auth(accessToken, { type: 'bearer' });
+
+      const { id, password, ...rest } = user;
+
+      expect(status).toBe(200);
+      expect(body).toEqual({
+        ok: true,
+        data: {
+          ...rest,
+          createdAt: new Date(user.createdAt).toISOString(),
+          updatedAt: new Date(user.updatedAt).toISOString(),
+        },
+      });
+    });
+  });
+
+  describe('/api/users/verify (PATCH)', () => {
+    it('Bad Request (400)', async () => {
+      const { status, body } = await request(app.getHttpServer())
+        .patch(`/api/users/verify`)
+        .auth(accessToken, { type: 'bearer' })
+        .send({ code: '123' });
+
+      expect(status).toBe(400);
+      expect(body).toEqual({
+        ok: false,
+        message: '인증코드가 유효하지 않습니다.',
+      });
+    });
+
+    it('Success (200)', async () => {
+      const verification = await prisma.verification.findFirst({
+        where: { userId: user.id },
+      });
+
+      const { status, body } = await request(app.getHttpServer())
+        .patch(`/api/users/verify`)
+        .auth(accessToken, { type: 'bearer' })
+        .send({ code: verification?.code });
+
+      expect(status).toBe(200);
+      expect(body).toEqual({
+        ok: true,
+      });
+
+      const count = await prisma.verification.count();
 
       expect(count).toBe(0);
     });
