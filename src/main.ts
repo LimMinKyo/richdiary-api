996e93d736ec65e778c8594b557a99cf94d10be6
LimMinKyo <story1400@naver.com>
2023-12-21 22:52:54 +0900

    ADD: CORS 설정

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  app.enableCors();
+  app.enableCors({
+    origin: [process.env.FRONT_URL || ''],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
